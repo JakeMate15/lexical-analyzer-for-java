1797f57aaecf1f3be613201b8a0cfe7757cf3177
JakeMate15 <erik.mofe51@gmail.com>
2023-05-22 18:11:50 -0600

    correccion operadores

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String [] args){
         int octal1 === -0123,octal2=0381;
         short dato = 12A12;
-        double PI = 3.1416,CteGrav=-6.67E-19;
+        double PI = 3.1416, CteGrav = -6.67E-19;
         float prom = (float) 0.0;
         double val;
-        Calculos generales 
-            jljkasdfjl
+        Calculos generales; 
+        jljkasdfjl;
         
-        for(int i=0; i<100; i++){
-            prom+=i;
+        //for ( int i = 0; i <= 100; i++ ) {
+            prom += i;
         }
+        
         double pot = 7.34E+1.4;
         val = CteGrav * dato * pot;
-        System.out.println("Prom = " + (prom/PI) + " Result = " + (val*0xAxB));
-        
+        System.out.println("Prom = " + (prom/PI) + " Result = " + (val * 0xAxB));
+        /*
     }
 }
